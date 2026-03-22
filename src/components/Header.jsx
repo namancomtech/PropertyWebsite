701b835de5d35b7e5fbb04444b74f230202720b8
Naman Sharma <namancomtech@gmail.com>
2026-03-22 15:55:55 +0530

    Added {Link} instead of href in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 export default function Header() {
   return (
       <Navbar expand="lg" className="bg-body-tertiary"  data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="/">Finest Properties</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Finest Properties</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav"  className="justify-content-end">
           <Nav >
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
