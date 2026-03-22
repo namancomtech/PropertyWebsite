50cefd9591a8926852b603cc4e42f7de9423f783
Naman Sharma <namancomtech@gmail.com>
2026-03-22 15:49:58 +0530

    Changed BrowserRouter into HashRouter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Index from "../src/components/Index"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import About from "../src/components/About"
 import Contact from "../src/components/Contact"
 function App() {
   return (
     <div>
-      <BrowserRouter basename="/PropertyWebsite">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
